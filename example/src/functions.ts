f2168118d9b52aeb38d461f8bfeab8ac6bc41e08
Gustavo <gustavopch@gmail.com>
2020-08-06 17:56:30 -0300

    fix: add ignoreUndefinedProperties flag in src/functions.ts

--- example/src/functions.ts
+++ example/src/functions.ts
@@ -7,6 +7,10 @@ import { views } from './views'
 
 const firebaseAdminApp = firebaseAdmin.initializeApp()
 
+firebaseAdminApp.firestore().settings({
+  ignoreUndefinedProperties: true,
+})
+
 module.exports = createFunctions(firebaseAdminApp, {
   domain,
   flows,
