237b17c0d489dba226e7da5d7dd08da53251f695
Gustavo <gustavopch@gmail.com>
2020-08-11 16:50:28 -0300

    feat: group latitude and longitude in coordinates field in Location

--- src/apis/https/utils/parse-location-from-headers.ts
+++ src/apis/https/utils/parse-location-from-headers.ts
@@ -3,21 +3,17 @@ import { Request } from 'express'
 import { Location } from '../../../elements/location'
 
 export const parseLocationFromHeaders = (req: Request): Location => {
-  let latitude: number | null = null
-  let longitude: number | null = null
-
+  let coordinates: { latitude: number; longitude: number } | null = null
   const latLongHeader = req.header('X-Appengine-CityLatLong')
   if (latLongHeader) {
-    const coordinates = latLongHeader.split(',').map(Number)
-    latitude = coordinates[0]
-    longitude = coordinates[1]
+    const [latitude, longitude] = latLongHeader.split(',').map(Number)
+    coordinates = { latitude, longitude }
   }
 
   return {
     city: req.header('X-Appengine-City') || null,
     region: req.header('X-Appengine-Region') || null,
     country: req.header('X-Appengine-Country') || null,
-    latitude,
-    longitude,
+    coordinates,
   }
 }
--- src/elements/location.ts
+++ src/elements/location.ts
@@ -2,6 +2,8 @@ export type Location = {
   city: string | null
   region: string | null
   country: string | null
-  latitude: number | null
-  longitude: number | null
+  coordinates: {
+    latitude: number
+    longitude: number
+  } | null
 }
