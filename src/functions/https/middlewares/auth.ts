d5a87c90dee55c43bdbc5f6c600fdb13de4d712a
Gustavo <gustavopch@gmail.com>
2020-10-06 14:42:05 -0300

    feat: implement API key authorization

--- src/functions/https/middlewares/auth.ts
+++ src/functions/https/middlewares/auth.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from 'express'
 import firebase from 'firebase-admin'
+import * as functions from 'firebase-functions'
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -20,21 +21,50 @@ export const auth = (firebaseApp: firebase.app.App): RequestHandler => {
     }
 
     const authorization = req.header('Authorization')
-    const [, idToken] = authorization?.split('Bearer ') ?? []
-    if (!idToken) {
-      console.log("Missing the 'Authorization' header:", authorization)
+    if (!authorization) {
+      console.log("Missing the 'Authorization' header")
       res.status(403).send('Unauthorized')
       return
     }
 
-    try {
-      const decodedIdToken = await firebaseApp.auth().verifyIdToken(idToken)
+    if (authorization.startsWith('ApiKey ')) {
+      const { apiKey } = functions.config()
+      if (!apiKey) {
+        console.warn("Missing the 'apiKey' environment configuration")
+        res.status(403).send('Unauthorized')
+        return
+      }
 
-      req.userId = decodedIdToken.uid
+      const [, incomingApiKey] = authorization.split('ApiKey ')
+      if (incomingApiKey !== apiKey) {
+        console.log(`Wrong API key: ${incomingApiKey}`)
+        res.status(403).send('Unauthorized')
+        return
+      }
+
+      req.userId = 'system'
       next()
-    } catch (error) {
-      console.log("Couldn't verify the Firebase ID token:", error)
-      res.status(403).send('Unauthorized')
+      return
     }
+
+    if (authorization.startsWith('Bearer ')) {
+      try {
+        const [, incomingIdToken] = authorization.split('Bearer ')
+        const decodedIdToken = await firebaseApp
+          .auth()
+          .verifyIdToken(incomingIdToken)
+        req.userId = decodedIdToken.uid
+        next()
+        return
+      } catch (error) {
+        console.log(`Couldn't verify the Firebase ID token: ${error}`)
+        res.status(403).send('Unauthorized')
+        return
+      }
+    }
+
+    console.log(`Unexpected 'Authorization' header: ${authorization}`)
+    res.status(403).send('Unauthorized')
+    return
   }
 }
