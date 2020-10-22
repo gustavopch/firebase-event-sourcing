3aa1aa492d77fda7140933324f4aef82ebe8ff01
Gustavo <gustavopch@gmail.com>
2020-10-22 19:23:53 -0300

    feat: allow passing string[] and null to AggregateService#exists

--- src/services/aggregates-service.ts
+++ src/services/aggregates-service.ts
@@ -2,7 +2,7 @@ import { EventStore } from '../stores/event-store'
 import { Aggregate } from '../types/aggregate'
 
 export type AggregatesService = {
-  exists: (aggregateId: string) => Promise<boolean>
+  exists: (aggregateId: string | string[] | null) => Promise<boolean>
   get: (aggregateId: string) => Promise<Aggregate | null>
 }
 
@@ -10,9 +10,20 @@ export const createAggregatesService = (
   eventStore: EventStore,
 ): AggregatesService => {
   return {
-    exists: async aggregateId => {
-      const aggregate = await eventStore.getAggregate(aggregateId)
-      return Boolean(aggregate)
+    exists: async aggregateIdOrIds => {
+      if (!aggregateIdOrIds) {
+        return false
+      }
+
+      const aggregateIds = Array.isArray(aggregateIdOrIds)
+        ? aggregateIdOrIds
+        : [aggregateIdOrIds]
+
+      const aggregates = await Promise.all(
+        aggregateIds.map(id => eventStore.getAggregate(id)),
+      )
+
+      return aggregates.every(Boolean)
     },
 
     get: async aggregateId => {
