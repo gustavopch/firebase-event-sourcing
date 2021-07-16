789b39386839d084b0b51e775295b0d92ea8dc03
Gustavo <gustavopch@gmail.com>
2021-07-16 12:30:00 -0300

    refactor: make userId non-optional in Request type

--- src/functions/https/middlewares/auth.ts
+++ src/functions/https/middlewares/auth.ts
@@ -6,7 +6,7 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
-      userId?: string
+      userId: string
     }
   }
 }
