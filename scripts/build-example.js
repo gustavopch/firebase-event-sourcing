0e5371501a7b232b75072c791fb6db782fd77292
Gustavo <gustavopch@gmail.com>
2021-07-28 20:41:03 -0300

    chore(example): replace Parcel with ESBuild

--- scripts/build-example.js
+++ scripts/build-example.js
@@ -1,25 +1,35 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process')
+
 const fs = require('fs')
 const { join } = require('path')
+const esbuild = require('esbuild')
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
 
-execSync(
-  'yarn parcel build ./example/src/functions.ts --out-dir ./example/dist --target node',
-  {
-    stdio: 'inherit',
-  },
-)
+const main = async () => {
+  await esbuild.build({
+    bundle: true,
+    entryPoints: ['./example/src/functions.ts'],
+    logLevel: 'info',
+    outdir: './example/dist',
+    platform: 'node',
+    plugins: [nodeExternalsPlugin()],
+    sourcemap: true,
+    target: 'node12',
+  })
 
-const packageJson = {
-  main: './functions',
-  dependencies: {
-    express: '4.17.1',
-    'firebase-admin': '9.0.0',
-    'firebase-functions': '3.9.0',
-  },
+  const packageJson = {
+    main: './functions',
+    dependencies: {
+      express: '4.17.1',
+      'firebase-admin': '9.0.0',
+      'firebase-functions': '3.9.0',
+    },
+  }
+
+  fs.writeFileSync(
+    join(process.cwd(), 'example', 'dist', 'package.json'),
+    JSON.stringify(packageJson),
+  )
 }
 
-fs.writeFileSync(
-  join(process.cwd(), 'example', 'dist', 'package.json'),
-  JSON.stringify(packageJson),
-)
+main()
